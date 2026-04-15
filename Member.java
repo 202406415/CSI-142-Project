9e6260ddd4094ab90135c1cc3146e8a2a3f7cbd5
lefatsheoratile <lefatsheoratile973@gmail.com>
2026-04-15 21:20:46 +0200

    Enhance Member class with contribution management
    Added getter and setter for contribution with validation.

--- Member.java
+++ Member.java
@@ -5,7 +5,16 @@ public class Member extends Person {
         super(name, id, cellphonoNo);
         this.contribution = contribution;
     } 
-   
+
+    public Contribution getContribution() {
+        return Contribution;
+    }
+    public void setContribution(Contribution contribution ) {
+        if(contribution.getAmount() == 0) {
+            throw new IllegalArgumentException("Contribution amount cannot be equals 0.");
+        }
+        this.contribution = contribution;
+        
     @Override
     public String getDetails(){
         return super.getDetails() + " Contribution:" + contribution.getDetails();
