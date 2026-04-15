3f0d32f673e793e40d3b5b4b1cbdb8dd8429efb5
lefatsheoratile <lefatsheoratile973@gmail.com>
2026-04-16 01:29:22 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,17 +1,17 @@
 public class Person{
     private String name;
     private String id;
-    private int cellPhoneNo;
+    private String cellPhoneNo;
 
     public Person(String name, String id,int cellPhoneNo){
         this.name = name;
         this.id = id;
-        this.cellPhoneNo = (cellPhoneNo);
+        this.cellPhoneNo =(cellPhoneNo);
     }
     public Person(String name,String id ) {
         this.name = name;
         this.id = id;
-        this.cellPhoneNo = 0;
+        this.cellPhoneNo = 'Not provided";
 
         public void setName(String name) {
             this.name;
@@ -20,19 +20,24 @@ public class Person{
         this.id = id;
     }
 
-    public void setCellPhoneNo(int cellPhoneNo ) {
-        if (String.valueOf(cellPhoneNo).length() !=8 ) {
+    public void setCellPhoneNo(String cellPhoneNo ){
+        if ( cellPhoneNo == null) {
+            throw new illegalArgumentException("Cellphone number cannot be null");
+        }  {
+        if (int i = 0; i < cellPhoneNo.length(); i++ ) {
             throw new IllegalArgumentException("Invalid cellphone number: must be exactly 8 digits(e.g., 72729838");
         }
-        if(cellPhoneNo < 0 ) {
-            throw new IllegalArgumentException("Invalid cellphone number: must be a positive number");
+        for(int i=0; i< cellPhoneNo.length(); i++) 
+        if( !character.isDigit(cellPhoneNo.charAt(i{
+            throw new IllegalArgumentException("Invalid cellphone number: must contain only diits 0-9"); }
         }
+    if(cellPhoneNo.chart(00 == '0') {
+    throw new IllegalArgumentException('Invalid cellphone number: first digit cannot be zero");
+}
         this.cellPhoneNo = cellPhoneNo;        
     }
     public String getDetails(){
         return "name: "+ name +" id: " + id +" Mogala: "+ cellPhoneNo;
     }
-    @Override
-    public String toString() {
-        return 'Person{name='" +name + ",id='" + id '",cellPhoneNo=" + cellPhoneNo +"}";
-}
+
+
