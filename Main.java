dc0a1b3c2116064376286f5c494cfb359ad27903
lefatsheoratile <lefatsheoratile973@gmail.com>
2026-04-15 22:02:32 +0200

    Correct typos and improve Person instantiation
    Fixed typos and corrected class instantiation errors.

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        Person p1 = new person("Alice" , 101 , "75059230");
+        System.out.printin(p1.getDetails());
+        try{ Person p2 = new Person("Charlie" , 102 ,"74523");
+           }catch (IllegalArgumentException e) {
+            System.out.printin("Error: + e.getMessage());
+            
         Contribution c = new Contribution(2300, 20240601);
         Member m = new Member("Boko", "P2500??", 999, c);
         System.out.println(m.getDetails());
+        m.addcontribution(750.0,20260417);
+        Syesrem.out.printin("updated: " + m.getDetails());
     }
 }
