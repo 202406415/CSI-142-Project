d6d5ae94109a8c57ad76278ba520be229c106f3c
lefatsheoratile <lefatsheoratile973@gmail.com>
2026-04-15 22:34:22 +0200

    Update cellphone number validation message

--- Person.java
+++ Person.java
@@ -5,7 +5,7 @@ public class Person{
 
     public Person(String name, String id,int cellphonoNo){
         if (String.valueOf(cellphonoNo).length() != 8) {
-            throw new IllegalArgumentException("Invalid cellphone number:must be exactly 8 digits.");
+            throw new IllegalArgumentException("Invalid cellphone number:must be exactly 8 digits and positive(e.g., 72729838.");
         }
         this.name = name;
         this.id = id;
