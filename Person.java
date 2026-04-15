681911e13e9cffdc88e93777a06a9da31ff4787c
lefatsheoratile <lefatsheoratile973@gmail.com>
2026-04-16 01:09:08 +0200

    Fix cellphone number field and validation logic
    Corrected spelling of 'cellPhoneNo' and updated related methods. Added validation for cellphone number length and positivity.

--- Person.java
+++ Person.java
@@ -1,22 +1,38 @@
 public class Person{
     private String name;
     private String id;
-    private int cellphonoNo;
+    private int cellPhoneNo;
 
-    public Person(String name, String id,int cellphonoNo){
-        if (String.valueOf(cellphonoNo).length() != 8) {
-            throw new IllegalArgumentException("Invalid cellphone number:must be exactly 8 digits and positive(e.g., 72729838.");
-        }
+    public Person(String name, String id,int cellPhoneNo){
         this.name = name;
         this.id = id;
-        this.cellphonoNo = cellphonoNo;
+        this.cellPhoneNo = (cellPhoneNo);
     }
     public Person(String name,String id ) {
         this.name = name;
         this.id = id;
-        this.cellphonoNo = 0;
+        this.cellPhoneNo = 0;
+
+        public void setName(String name) {
+            this.name;
+    }
+    public void setid (String id) {
+        this.id = id;
+    }
+
+    public void setCellPhoneNo(int cellPhoneNo ) {
+        if (String.valueOf(cellPhoneNo).length() !=8 ) {
+            throw new IllegalArgumentException("Invalid cellphone number: must be exactly 8 digits(e.g., 72729838");
+        }
+        if(cellPhoneNo < 0 ) {
+            throw new IllegalArgumentException("Invalid cellphone number: must be a positive number");
+        }
+        this.cellPhoneNo = cellPhoneNo;        
     }
     public String getDetails(){
-        return "name: "+ name +" id: " + id +" Mogala: "+ cellphonoNo;
+        return "name: "+ name +" id: " + id +" Mogala: "+ cellPhoneNo;
     }
+    @Override
+    public String toString() {
+        return 'Person{name='" +name + ",id='" + id '",cellPhoneNo=" + cellPhoneNo +"}";
 }
