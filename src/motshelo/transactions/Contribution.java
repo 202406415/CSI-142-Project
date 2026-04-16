564f9cfcc9611a32bb56a8c49b9e684e0b125aa4
Nomsa Molatlhegi <salsa123.angel@gmail.com>
2026-04-16 14:12:21 +0200

    Update Contribution.java

--- src/motshelo/transactions/Contribution.java
+++ src/motshelo/transactions/Contribution.java
@@ -3,15 +3,20 @@ package motshelo.transactions;
 public class Contribution{
     private double amount;
     private int date;
+    private String id;
 
-    public Contribution(double amount, int date){
+    public Contribution(double amount, int date, String id){
         this.amount = amount;
         this.date = date;
+        this.id = id;
     }
     public double getAmount() {
         return amount;
     }
+    public String getId() {
+        return id;
+     }
     public String getDetails() {
-        return "Ontshitse P" + amount + " kadi " + date;
+    return "Amount: " + amount + " Date: " + date + " ID: " + id;
     }
 }
