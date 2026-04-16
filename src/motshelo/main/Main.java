93b755b25ce1e8f5c57099266b89287c2dae97ab
Nomsa Molatlhegi <salsa123.angel@gmail.com>
2026-04-16 14:14:40 +0200

    Update Main.java
    Added a menu

--- src/motshelo/main/Main.java
+++ src/motshelo/main/Main.java
@@ -1,9 +1,47 @@
  package motshelo.main;
+import java.util.Scanner;
+import motshelo.model.Member;
+import motshelo.transactions.Contribution;
 
 public class Main {
     public static void main(String[] args) {
-        Contribution c = new Contribution(2300, 20240601);
-        Member m = new Member("Boko", "P2500??", 999, c);
+        Scanner in = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n==== MOTSHELO MENU =====");
+            System.out.println("1. Create Member");
+            System.out.println("2. Exit");
+            System.out.println("Choose Option: ");
+             
+            choice = in.nextInt();
+            in.nextLine();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter name: ");
+                    String name = in.nextLine();
+                System.out.println("Enter ID: ");
+                    String id = in.nextLine();
+                System.out.println("Enter Cellphone Number: ");
+                    int cellphonoNo = in.nextInt();
+                System.out.println("Enter Contribution amount: ");
+                    double amount = in.nextDouble();
+                System.out.println("Enter date: ");
+                    int date = in.nextInt();            
+
+        Contribution c = new Contribution(amount, date, id);
+        Member m = new Member(name, id, cellphonoNo, c);
+        System.out.println("\nMember Details:");
         System.out.println(m.getDetails());
+            break;
+        case 2:
+            System.out.println("Exiting system........");
+            break;
+        default:
+            System.out.println("Invalid Option.");    
+        }
+      } while (choice != 2);
+        
     }
 }
+
